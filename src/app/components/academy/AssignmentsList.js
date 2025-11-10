44b927959011165fbd0c870951d97e9f46e18722
pradeep-ravan <triplehthegame143@gmail.com>
2025-11-10 23:28:26 +0530

    done with asiignment flow

--- src/app/components/academy/AssignmentsList.js
+++ src/app/components/academy/AssignmentsList.js
@@ -149,13 +149,21 @@ const AssignmentsList = () => {
                       placeholder="Search assignments..."
                       value={searchTerm}
                       onChange={(e) => setSearchTerm(e.target.value)}
+                      style={{ paddingRight: '40px' }}
                     />
                     <Button
                       type="button"
                       className="btn btn-trans border-0 bg-transparent p-0 m-0 position-absolute"
-                      style={{ right: '10px', top: '50%', transform: 'translateY(-50%)' }}
+                      style={{ 
+                        right: '10px', 
+                        top: '50%', 
+                        transform: 'translateY(-50%)',
+                        pointerEvents: 'none',
+                        zIndex: 1
+                      }}
+                      disabled
                     >
-                      <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ pointerEvents: 'none' }}>
                         <path d="M17.4995 17.5L13.8828 13.8833" stroke="#505050" strokeLinecap="round" strokeLinejoin="round" />
                         <path d="M9.16667 15.8333C12.8486 15.8333 15.8333 12.8486 15.8333 9.16667C15.8333 5.48477 12.8486 2.5 9.16667 2.5C5.48477 2.5 2.5 5.48477 2.5 9.16667C2.5 12.8486 5.48477 15.8333 9.16667 15.8333Z" stroke="#505050" strokeLinecap="round" strokeLinejoin="round" />
                       </svg>
