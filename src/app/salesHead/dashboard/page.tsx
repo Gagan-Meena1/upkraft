92311f5a8140af7f9f037287ba18ae1e8918996e
Gagan Meena <meenagagan122@gmail.com>
2026-05-20 22:57:00 +0530

    removed the sidebar

--- src/app/salesHead/dashboard/page.tsx
+++ src/app/salesHead/dashboard/page.tsx
@@ -156,7 +156,7 @@ export default function SalesHeadDashboard() {
   return (
     <div className="dashboard-root">
       <div className="dashboard-body">
-        <Sidebar />
+        {/* <Sidebar /> */}
         <div className="main">
           <TopNav />
           {/* <StatsRow /> */}
