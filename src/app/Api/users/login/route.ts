bf2872c797685a32ca019fdfbc35d112b8365fa5
Gagan Meena <meenagagan122@gmail.com>
2026-07-17 16:47:08 +0530

    added the bearer token for the app

--- src/app/Api/users/login/route.ts
+++ src/app/Api/users/login/route.ts
@@ -47,6 +47,7 @@ export async function POST(request: NextRequest) {
     console.log("[Token generated]");
     const response = NextResponse.json({
       message: "Login successful",
+      token,
       user: {
         id: user._id,
         email: user.email,
